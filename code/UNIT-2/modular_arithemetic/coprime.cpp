423fd36e46e54ee2e12d1bc544128fdf07fcd0d8
kshitij <kpandey0022@gmail.com>
2026-06-02 15:25:13 +0530

    Coprime

---
+++ code/UNIT-2/modular_arithemetic/coprime.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b){
+    if(a%b==0)
+    return b;
+    return gcd(b,a%b);
+}
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    if(gcd(a,b)==1)
+        cout<<"Coprime"<<endl;
+    else
+        cout<<"Not Coprime"<<endl;
+    return 0;
+}
--- code/UNIT-2/modular_arithemetic/mod_exp.cpp
+++ code/UNIT-2/modular_arithemetic/mod_exp.cpp
@@ -1,8 +1,22 @@
 #include <bit/stdc++.h>
 using namespace std;
 
+long long mod_exp(long long b,long long e,long long m){
+    long long answer=1;
+    b%=m;
+    while(e>0){
+        if(e&1)
+            answer=(answer*b)%m;
+        b=(b*b)%m;
+        e>>=1;
+    }
+    return answer;
+}
+
 int main(){
     int b,e,m;
     cin>>b>>e>>m;
-    
+    long long answer=mod_exp(b,e,m);
+    cout<<answer;
+    return 0;
 }
