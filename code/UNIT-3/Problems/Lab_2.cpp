539be9d68ba53bfe27f1468226c35aacbf793ea8
kshitij <kpandey0022@gmail.com>
2026-06-03 12:44:20 +0530

    Lab-2

--- code/UNIT-3/Problems/Lab_2.cpp
+++ code/UNIT-3/Problems/Lab_2.cpp
@@ -4,24 +4,65 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    vector<int>link(n);
-    vector<int>xor(n);
-    for(auto &x:link){
-        cin>>x;
-        
+
+    vector<int> value(n+1);
+    vector<int> link(n+1);
+
+    for(int i=1;i<=n;i++){
+        cin>>value[i];
+    }
+
+    // Build XOR links
+    for(int i=1;i<=n;i++){
+        int prev=i-1;
+        int next=i+1;
+
+        if(i==1)
+            prev=0;
+
+        if(i==n)
+            next=0;
+
+        link[i]=prev^next;
+    }
+
+    // Forward Traversal
+    cout<<"Forward: ";
+
+    int prev=0;
+    int cur=1;
+
+    while(cur!=0){
+        cout<<value[cur]<<" ";
+
+        int next=link[cur]^prev;
+        prev=cur;
+        cur=next;
     }
     cout<<endl;
 
-    cout<<"Forward Links: "<<endl;
-    for(int i=0;i<n;i++){
-        cout<<i<<" ";
+    // Backward Traversal
+    cout<<"Backward: ";
+
+    prev=0;
+    cur=n;
+
+    while(cur!=0){
+        cout<<value[cur]<<" ";
+
+        int next=link[cur]^prev;
+        prev=cur;
+        cur=next;
     }
     cout<<endl;
 
-    cout<<"Backward Links: "<<endl;
-    for(auto &x:link) 
-        cout<<x<<" ";
+    // XOR Links
+    cout<<"XOR Links: ";
+
+    for(int i=1;i<=n;i++){
+        cout<<link[i]<<" ";
+    }
     cout<<endl;
 
-
+    return 0;
 }
