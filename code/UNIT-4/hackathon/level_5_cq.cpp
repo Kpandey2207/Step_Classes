bab1d07ce817e26de18fdfd300e266f10f56c6e8
kshitij <kpandey0022@gmail.com>
2026-06-04 14:19:37 +0530

    Monotonc dequeue

---
+++ code/UNIT-4/hackathon/level_5_cq.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void enqueue(vector<string>&buffer, int 
+
+int main(){
+    int C, n;
+    cin>>C>>n;
+    vector<int> buffer(C);
+    int front=0;
+    int curr=0;
+    int overwrite=0;
+    while(n--){
+        string op;
+        int id;
+        cin>>op>>id;
+        int rear=(front+curr)%C;
+        buffer[rear]=id;
+        if(curr<C)
+            curr++;
+        else{
+            front=(front+1)%C;
+            overwrite++;
+        }
+    }
+    cout<<"Buffer: ";
+    for(int i=0;i<curr;i++){
+        int idx=(front+i)%C;
+        cout<<buffer[idx]<<" ";
+    }
+    cout<<"\nOverwrite Count: "<<overwrite<<endl;
+}
